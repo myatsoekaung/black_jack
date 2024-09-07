185734bb7af686d8a9bd9a594034eb11a1a53c7f
Myat Soe Kaung <myatsoekaung0000@gmail.com>
2024-09-07 20:18:05 +0700

    html file

---
+++ script.js
@@ -0,0 +1,27 @@
+let firstCard = 10
+let secondCard = 4
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+let messageEl = document.getElementById("message-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("cards-el");
+
+function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card? 🙂"
+    } else if (sum === 21) {
+        message = "Wohoo! You've got Blackjack! 🥳"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! 😭"
+        isAlive = false
+    }
+    messageEl.textContent = message;
+    
+}
